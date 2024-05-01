c321b4bc852f9b3a70282d480b553c291455c42b
johnl <johnl@DESKTOP-PESCMDO.lan>
2024-05-01 18:41:19 +0800

    new project collab

--- src/TRial.java
+++ src/TRial.java
@@ -3,5 +3,6 @@ public class TRial {
     public static void main(String[] args) {
         
         System.out.println("We are going to collab using net beans");
+        System.out.println("Send reply if you are connected to my project");
     }
 }
