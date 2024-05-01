31d8eb9e15268f355a055533b0874055892c6117
Administrator <Administrator@Luther.lan>
2024-05-01 18:44:12 +0800

    I received your message

--- src/TRial.java
+++ src/TRial.java
@@ -4,5 +4,6 @@ public class TRial {
         
         System.out.println("We are going to collab using net beans");
         System.out.println("Send reply if you are connected to my project");
+        System.out.println("I received your message thank you for inviting me in this project");
     }
 }
